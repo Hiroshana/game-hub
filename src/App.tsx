1bb461664897e4bb9aaca2888fa51d3abad9853f
Hiroshana <hiroshanawanni@gmail.com>
2026-02-08 12:01:47 +1100

    Built Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import { Button, ButtonGroup, HStack } from "@chakra-ui/react";
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+
 function App() {
+  const showAside = useBreakpointValue({ base: false, lg: true });
+
   return (
-    <HStack>
-      <Button colorPalette="blue">Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <Grid
+      templateAreas={{
+        base: `
+      "nav"
+      "main"
+    `,
+        lg: `
+      "nav nav"
+      "aside main"
+    `,
+      }}
+      gridTemplateColumns={{
+        base: "1fr",
+        lg: "300px 1fr", // aside width + main
+      }}
+    >
+      <GridItem area="nav" bg="blue.400">
+        Nav
+      </GridItem>
+
+      {showAside && (
+        <GridItem area="aside" bg="green.400">
+          Aside
+        </GridItem>
+      )}
+
+      <GridItem area="main" bg="gray.200">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
