452e9e2bbc43c2c407ad6c19a1ec9ac63a85965f
Hiroshana <hiroshanawanni@gmail.com>
2026-02-10 15:18:24 +1100

    Move game title to bottom

--- src/components/hub/GameCard.tsx
+++ src/components/hub/GameCard.tsx
@@ -13,15 +13,15 @@ const GameCard = ({ game }: Props) => {
     <Card.Root>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <Card.Body>
-        <Card.Header fontSize={"xl"} paddingLeft={0}>
-          {game.name}
-        </Card.Header>
         <HStack justifyContent={"space-between"}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Card.Header fontSize={"xl"} paddingLeft={0}>
+          {game.name}
+        </Card.Header>
       </Card.Body>
     </Card.Root>
   );
