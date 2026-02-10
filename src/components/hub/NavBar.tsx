b8e223c60f2923495187d48a588aee0b9aa8a36b
Hiroshana <hiroshanawanni@gmail.com>
2026-02-10 13:00:56 +1100

    Build search component

--- src/components/hub/NavBar.tsx
+++ src/components/hub/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../../assets/game-hub-logo.jpg";
 import ToggleForDarkLight from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding={"10px"}>
+    <HStack padding={"10px"} gap={10}>
       <Image src={logo} boxSize="60px" borderRadius={10} />
+      <SearchInput />
       <ToggleForDarkLight></ToggleForDarkLight>
     </HStack>
   );
---
+++ src/components/hub/SearchInput.tsx
@@ -0,0 +1,17 @@
+import { InputGroup } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react/input";
+import { BsSearch } from "react-icons/bs";
+
+const SearchInput = () => {
+  return (
+    <InputGroup startElement={<BsSearch />}>
+      <Input
+        borderRadius={10}
+        placeholder="Search games..."
+        variant={"subtle"}
+      />
+    </InputGroup>
+  );
+};
+
+export default SearchInput;
