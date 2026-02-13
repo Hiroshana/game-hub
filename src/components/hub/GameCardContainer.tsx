f4318cf8b79dd018fb590175cb108964de0b0582
Hiroshana <hiroshanawanni@gmail.com>
2026-02-13 23:27:56 +1100

    Fetch Game details

--- src/components/hub/GameCardContainer.tsx
+++ src/components/hub/GameCardContainer.tsx
@@ -6,7 +6,14 @@ interface Props {
 }
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow={"hidden"}>
+    <Box
+      overflow={"hidden"}
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+      borderRadius={10}
+    >
       {children}
     </Box>
   );
