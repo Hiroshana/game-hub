49d23d26a8ae2d7e44811c8d68d77d62b2074c7f
Hiroshana <hiroshanawanni@gmail.com>
2026-02-19 12:44:08 +1100

    Fix navbar

--- src/components/hub/NavBar.tsx
+++ src/components/hub/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../../assets/game-hub-logo.jpg";
 import ToggleForDarkLight from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding={"10px"} gap={10}>
-      <Image src={logo} boxSize="60px" borderRadius={10} />
+      <Link to="/">
+        <Image src={logo} width="80px" borderRadius={10} objectFit="cover" />
+      </Link>
       <SearchInput />
       <ToggleForDarkLight></ToggleForDarkLight>
     </HStack>
--- src/components/hub/SearchInput.tsx
+++ src/components/hub/SearchInput.tsx
@@ -3,17 +3,23 @@ import { InputGroup } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react/input";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+
+  const navigate = useNavigate();
 
   return (
     <form
       style={{ width: "100%" }}
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) setSearchText(ref.current.value);
+        if (ref.current) {
+          setSearchText(ref.current.value);
+          navigate("/");
+        }
       }}
     >
       <InputGroup startElement={<BsSearch />}>
